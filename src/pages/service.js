a6eb319c17dad4d643726583a643c45c4d8e8328
Vaibhav Nadgonde <itgenesys@gmail.com>
2020-05-16 15:53:16 +0530

    bug fix

--- src/pages/service.js
+++ src/pages/service.js
@@ -4,16 +4,14 @@ import Layout from "../containers/layout/layout";
 import Banner from "../containers/company/banner";
 import About from "../containers/company/about";
 import Story from "../containers/home-particle/story";
-import TestimonialSection from "../containers/company/testimonial";
-import ClientSection from "../containers/home-particle/clients";
 
 export default () => (
   <Layout>
     <SEO title="Service" />
     <Banner />
     <Story />
-    <TestimonialSection />
-    <ClientSection />
+    <Story />
+    <Story />
     <About />
   </Layout>
 );
