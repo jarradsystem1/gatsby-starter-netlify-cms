016688d12e985fe7d1d5ad43ef4653f3be957518
Vaibhav Nadgonde <itgenesys@gmail.com>
2020-05-18 19:58:19 +0530

    bug fix

--- src/pages/project.js
+++ src/pages/project.js
@@ -3,16 +3,13 @@ import SEO from "../components/seo";
 import Layout from "../containers/layout/layout";
 import Story from "../containers/home-particle/story";
 import Banner from "../containers/company/banner";
-import About from "../containers/company/about";
-import Awwards from "../containers/company/about/awwards";
 
 export default () => (
   <Layout>
     <SEO title="Project" />
     <Banner />
     <Story />
-    <Awwards />
-    <Awwards />
-    <Awwards />
+    <Story />
+    <Story />
   </Layout>
 );
